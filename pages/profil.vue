a12bb9e054a018e824f2da38b48711f665cb7202
snturk <muratcansenturk2000@hotmail.com>
2020-07-23 21:30:37 +0300

    added profile info

--- pages/profil.vue
+++ pages/profil.vue
@@ -2,13 +2,20 @@
   <div class="container">
     <div id="profileTitle">Profiliniz</div>
     <hr>
-    <div id="profileContent">
-      <div id="profileSummaryContainer">
+    <div id="profileContentContainer">
+      <div class="profileContent" id="profileSummaryContainer">
         <div class="profileSubtitle">Özet</div>
         <div class="summaryElement">Toplam firma: <span>{{totalCompanies}}</span></div>
         <div class="summaryElement">Toplam iş: <span>{{totalJobs}}</span></div>
         <div class="summaryElement">Tamamlanan iş: <span>{{totalJobs-totalTodo}}</span></div>
         <div class="summaryElement">Kalan iş: <span>{{totalTodo}}</span></div>
+      </div>
+
+      <div class="profileContent" id="profileInfoContainer">
+        <div class="profileSubtitle">Profil Bilgileri</div>
+        <div class="info"> {{user.displayName}} </div>
+        <div class="info"> {{user.email}} </div>
+        <div class="info"></div>
       </div>
     </div>
   </div>
@@ -16,9 +23,11 @@
 
 <script>
 import { mapState } from 'vuex'
+import firebase from 'firebase'
 export default {
   data() {
     return {
+      user: firebase.auth().currentUser,
       totalCompanies: this.$store.state.totalCompanies,
       totalJobs: this.$store.state.totalJobs,
       totalTodo: this.$store.state.totalTodo
@@ -43,7 +52,7 @@ hr{
   width: 30%;
 }
 
-#profileContent{
+#profileContentContainer{
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -51,17 +60,23 @@ hr{
   width: 90%;
 }
 
+.profileContent{
+  padding: 20px;
+  min-height: 200px;
+  height: 30%;
+  border: 1px solid hsl(245, 30%, 35%);
+  border-radius: 7px;
+  box-shadow: 0 4px 6px rgba(0,0,0,0.30), 0 4px 5px rgba(0, 0, 0, 0.32);
+}
+
 #profileSummaryContainer{
   margin-top: 3%;
+  width: 25%;
   text-align: left;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   font-weight: 400;
-  padding: 20px;
-  border: 1px solid hsl(245, 30%, 35%);
-  border-radius: 7px;
-  box-shadow: 0 4px 6px rgba(0,0,0,0.30), 0 4px 5px rgba(0, 0, 0, 0.32);
 }
 .profileSubtitle{
   font-size: 26px;
