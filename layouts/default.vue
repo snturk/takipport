ba5215e58382dab7babe381651a521f05499547f
snturk <muratcansenturk2000@hotmail.com>
2020-07-24 12:14:16 +0300

    editted height props

--- layouts/default.vue
+++ layouts/default.vue
@@ -192,8 +192,8 @@ a{
   top: 0;
   left: 0;
   min-height: 100%;
-  height: fit-content;
-  width: 100%;
+  height: 100vh;
+  width: 100vw;
   display: flex;
   flex-direction: column;
   align-items: center;
